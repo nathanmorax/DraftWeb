d1320d92571656898c74b9feac9fcc26349970b9
Jonathan Mora <nathanmorax@outlook.com>
2025-07-23 23:50:31 -0600

    Initial commit

---
+++ DraftWeb/DraftWebApp.swift
@@ -0,0 +1,17 @@
+//
+//  DraftWebApp.swift
+//  DraftWeb
+//
+//  Created by Jonathan Mora on 23/07/25.
+//
+
+import SwiftUI
+
+@main
+struct DraftWebApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
