1a1abe181ac2815e1aa1bb92c593c1b6a370294e
Jonathan Mora <nathanmorax@outlook.com>
2025-07-24 00:07:39 -0600

    Add page list and URL submission to ContentView
    Introduced a Store class to manage a list of Page objects, each representing a URL. Updated ContentView to display the list of submitted URLs in a sidebar and added a TextField in the toolbar for submitting new URLs.

--- DraftWeb/ContentView.swift
+++ DraftWeb/ContentView.swift
@@ -6,16 +6,49 @@
 //
 
 import SwiftUI
+import Observation
 
+struct Page: Identifiable, Hashable {
+    var id = UUID()
+    var url: URL
+}
+@Observable
+class Store {
+    var pages: [Page] = []
+    
+    func submit(_ url: URL) {
+        
+        pages.append(Page(url: url))
+        
+    }
+    
+}
 struct ContentView: View {
+    
+    @State var store = Store()
+    @State var currentURLString: String = "https://www.objc.io"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationSplitView(sidebar: {
+            List {
+                ForEach(store.pages) { page in
+                    Text(page.url.absoluteString)
+                }
+            }
+        }, detail: {
+            Text("Detail View")
+        })
+        .toolbar {
+            ToolbarItem(placement: .principal) {
+                TextField("URL" ,text: $currentURLString)
+                    .onSubmit {
+                        if let url = URL(string: currentURLString) {
+                            store.submit(url)
+                            currentURLString = ""
+                        }
+                    }
+            }
         }
-        .padding()
     }
 }
 
