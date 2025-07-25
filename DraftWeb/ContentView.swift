9ab86e56cb4c714b8829c6a5ad07d83c1e231754
Jonathan Mora <nathanmorax@outlook.com>
2025-07-24 20:26:58 -0600

    Add web page snapshot functionality
    Introduces a snapshot feature to WebView and ContentView, allowing users to take and display a snapshot of the current web page. Also initializes Store with default pages and updates UI to show the snapshot image overlay.

--- DraftWeb/ContentView.swift
+++ DraftWeb/ContentView.swift
@@ -15,8 +15,12 @@ struct Page: Identifiable, Hashable {
 }
 @Observable
 class Store {
-    var pages: [Page] = []
-
+    var pages: [Page] = [
+        .init(url: .init(string: "https://www.objc.io")!),
+        .init(url: .init(string: "https://www.apple.com")!)
+        
+    ]
+    
     func submit(_ url: URL) {
         
         pages.append(Page(url: url))
@@ -27,6 +31,7 @@ class Store {
 struct WebView: NSViewRepresentable {
     
     var url: URL
+    var snapShot: (_ takeSnapshot: @escaping () async -> NSImage) -> ()
     
     class Coordinator: NSObject, WKNavigationDelegate {
         
@@ -43,6 +48,11 @@ struct WebView: NSViewRepresentable {
     }
     
     func updateNSView(_ nsView: WKWebView, context: Context) {
+        assert(Thread.isMainThread)
+        snapShot({
+            assert(Thread.isMainThread)
+            return try! await nsView.takeSnapshot(configuration: nil)
+        })
         
         if nsView.url != url {
             let request = URLRequest(url: url)
@@ -51,12 +61,17 @@ struct WebView: NSViewRepresentable {
     }
 }
 
+// TODO: - Check in Pattern Proxy( remove)
+extension NSImage: @unchecked Sendable { }
 
 struct ContentView: View {
     
     @State var store = Store()
     @State var currentURLString: String = "https://www.objc.io"
     @State var selectedPage: Page.ID?
+    @State var image: NSImage?
+    @State var takeSnapshot: (() async -> NSImage)?
+    
     
     var body: some View {
         NavigationSplitView(sidebar: {
@@ -68,8 +83,17 @@ struct ContentView: View {
         }, detail: {
             
             if let s = selectedPage, let page = store.pages.first(where: { $0.id == s }) {
-                WebView(url: page.url)
-
+                WebView(url: page.url, snapShot: { takeSnapshot in
+                    self.takeSnapshot = takeSnapshot
+                })
+                .overlay {
+                    if let i = image {
+                        Image(nsImage: i)
+                            .scaleEffect(0.5)
+                            .border(Color.red)
+                    }
+                }
+                
             } else {
                 ContentUnavailableView("No Page Selected", systemImage: "globe")
             }
@@ -81,9 +105,17 @@ struct ContentView: View {
                     .onSubmit {
                         if let url = URL(string: currentURLString) {
                             currentURLString = ""
-                                store.submit(url)
+                            store.submit(url)
                         }
                     }
+            }
+            
+            ToolbarItem {
+                Button("Take a Snapshot") {
+                    Task {
+                        image = await takeSnapshot?()
+                    }
+                }
             }
         }
     }
